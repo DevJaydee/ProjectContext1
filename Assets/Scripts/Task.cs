76c1ca85070ac6b2821ab97de3a2a92656116e42
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-09 21:59:19 +0100

    Add some better visual feedback.
    With this update the counter adds an "0" in-front of the string if it's lower than 10. This is done because it looks more like a timer that way.

--- Assets/Scripts/Task.cs
+++ Assets/Scripts/Task.cs
@@ -20,13 +20,15 @@ public class Task : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IP
 	[SerializeField] private new string name = "";  // Name of the task.
 	[SerializeField] private string description = "";   // Description for the task.
 	[SerializeField] private int dueDateMinutes = 0;  // How many Minutes are left before the task is due.
-	[SerializeField] private int dueDataHours = 0;    // How many Hours are left before the task is due.
+	[SerializeField] private int dueDateHours = 0;    // How many Hours are left before the task is due.
 	[SerializeField] private int dueDateDays = 0;     // How many Days are left before the task is due.
 	[Space]
 	[SerializeField] private Image image = default;   // The image for the task. This will be some type of food.
 	[SerializeField] private TextMeshProUGUI dueDateCounter = default;  // The text element that shows the user how much time is left.
 
 	private float convertedMinutesToSecondDeadline = 0;
+	private float convertedHoursToSecondsDeadline = 0;
+	private float convertedDaysToSecondsDeadline = 0;
 	#endregion
 
 	#region Getters And Setters
@@ -35,7 +37,7 @@ public class Task : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IP
 	public string Name { get => name; set => name = value; }
 	public string Description { get => description; set => description = value; }
 	public int DueDateMinutes { get => dueDateMinutes; set => dueDateMinutes = value; }
-	public int DueDataHours { get => dueDataHours; set => dueDataHours = value; }
+	public int DueDataHours { get => dueDateHours; set => dueDateHours = value; }
 	public int DueDateDays { get => dueDateDays; set => dueDateDays = value; }
 
 	public Image Image { get => image; set => image = value; }
@@ -46,6 +48,8 @@ public class Task : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IP
 	private void Start()
 	{
 		convertedMinutesToSecondDeadline = dueDateMinutes * 60;
+		convertedHoursToSecondsDeadline = dueDateHours * 3600;
+		convertedDaysToSecondsDeadline = dueDateDays * 86400;
 	}
 
 	private void Update()
@@ -53,8 +57,20 @@ public class Task : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IP
 		if(state == TaskState.Active)
 		{
 			convertedMinutesToSecondDeadline -= Time.deltaTime;
+			convertedHoursToSecondsDeadline -= Time.deltaTime;
+			convertedDaysToSecondsDeadline -= Time.deltaTime;
 
-			dueDateCounter.text = dueDateDays + ":" + dueDataHours + ":" + (convertedMinutesToSecondDeadline / 60).ToString("F0");
+			string dueDateDaysString = (convertedDaysToSecondsDeadline / 86400).ToString("F0");
+			if(dueDateDays < 10) dueDateDaysString = "0" + dueDateDaysString;
+
+			string dueDateHoursString = (convertedHoursToSecondsDeadline / 3600).ToString("F0");
+			if(dueDateHours < 10) dueDateHoursString = "0" + dueDateHoursString;
+
+			string dueDateMinutesString = (convertedMinutesToSecondDeadline / 60).ToString("F0");
+			if(dueDateMinutes < 10) dueDateMinutesString = "0" + dueDateMinutesString;
+
+
+			dueDateCounter.text = dueDateDaysString + ":" + dueDateHoursString + ":" + dueDateMinutesString;
 		}
 	}
 
