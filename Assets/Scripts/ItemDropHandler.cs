3efbb8692717e8977be116db49c1957f7de451ed
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-10 15:45:47 +0100

    Add ItemDropHandler.cs
    This script handles everything that should happen when an item is dropped.

---
+++ Assets/Scripts/ItemDropHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class ItemDropHandler : MonoBehaviour, IDropHandler
+{
+	public void OnDrop(PointerEventData eventData)
+	{
+		if(eventData.pointerDrag != null)
+		{
+			Destroy(eventData.pointerDrag);
+			Debug.Log("Dropped on the Monster!");
+		}
+	}
+}
