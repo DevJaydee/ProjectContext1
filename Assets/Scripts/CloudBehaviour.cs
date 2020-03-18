b989e2ee5f082a462655985245125dd39a790541
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-18 21:40:44 +0100

    Add Cloud Behaviour

---
+++ Assets/Scripts/CloudBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBehaviour : MonoBehaviour
+{
+	[SerializeField] private GameObject character = default;
+
+	private BoxCollider2D bc2d = default;
+
+	private void Start()
+	{
+		bc2d = GetComponent<BoxCollider2D>();
+	}
+
+	private void Update()
+	{
+		if(character.transform.position.y < transform.position.y )
+		{
+
+		}
+	}
+}
