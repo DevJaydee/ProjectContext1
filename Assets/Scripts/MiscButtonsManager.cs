9737e4f1ecff868c594330ef40ab25cf23c0d328
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-18 21:41:23 +0100

    Add Misc Button Manager

---
+++ Assets/Scripts/MiscButtonsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MiscButtonsManager : MonoBehaviour
+{
+	[SerializeField] private GameObject miniGamesMenu = default;        // Reference to the MiniGamesMenu gameobject.
+
+	private void Start()
+	{
+		miniGamesMenu.SetActive(false);
+	}
+
+	public void ToggleMiniGamesMenu()
+	{
+		miniGamesMenu.SetActive(!miniGamesMenu.activeInHierarchy);
+	}
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
