454a92575c39679286f56af2ebc1b468671008be
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-18 21:41:50 +0100

    Add On Scene Load Change Rotation
    This changes the rotation of the Device depending on which scene is loaded.

---
+++ Assets/Scripts/OnSceneLoadChangeRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Rotation
+{
+	Landscape,
+	Portrait
+}
+
+public class OnSceneLoadChangeRotation : MonoBehaviour
+{
+	[SerializeField] private Rotation rotation = Rotation.Landscape;
+
+	private void Awake()
+	{
+		switch(rotation)
+		{
+			case Rotation.Landscape:
+				Screen.orientation = ScreenOrientation.LandscapeLeft;
+				break;
+			case Rotation.Portrait:
+				Screen.orientation = ScreenOrientation.Portrait;
+				break;
+			default:
+				break;
+		}
+	}
+}
