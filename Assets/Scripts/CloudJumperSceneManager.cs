dbaa94c756186f11ec0e9698a8fd03287d256277
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-20 13:03:55 +0100

    Add Cloud Jumper Scene Manager

---
+++ Assets/Scripts/CloudJumperSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudJumperSceneManager : MonoBehaviour
+{
+	#region Variables
+	private static CloudJumperSceneManager instance = null;
+
+	[SerializeField] private GameObject cloudJumperCharacter = default;     // Reference to the Cloud Jumper Character.
+	#endregion
+
+	#region Getters & Setters
+	public static CloudJumperSceneManager Instance { get => instance; set => instance = value; }
+	public GameObject CloudJumperCharacter { get => cloudJumperCharacter; set => cloudJumperCharacter = value; }
+	#endregion
+
+	#region Monobehaviour Callbacks
+	private void Awake()
+	{
+		if(!instance || instance != this)
+			instance = this;
+	}
+	#endregion
+}
