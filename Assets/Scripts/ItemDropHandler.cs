7b24b25c630c6ceeb5c44d642de8af5dcae11dbb
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-20 13:04:11 +0100

    Update To handle new food hunger functionality

--- Assets/Scripts/ItemDropHandler.cs
+++ Assets/Scripts/ItemDropHandler.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class ItemDropHandler : MonoBehaviour, IDropHandler
 {
+	[SerializeField] private CharacterBehaviour character = default;
+
 	public void OnDrop(PointerEventData eventData)
 	{
 		if(eventData.pointerDrag != null)
 		{
+			character.IncreaseHunger(eventData.pointerDrag.GetComponent<Task>().TaskHungerWorth);
 			Destroy(eventData.pointerDrag);
 			Debug.Log("Dropped on the Monster!");
 		}
