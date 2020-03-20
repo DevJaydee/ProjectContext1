0a153ae228256bbee8524e4e5fd9b9b168fa2023
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-20 13:04:17 +0100

    Add Smoothfollow

---
+++ Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+	[SerializeField] private Transform target = default;
+	[SerializeField] private float smoothing = 7.5f;
+	[SerializeField] private Vector3 offset = Vector3.zero;
+
+	private Vector3 desiredPos;
+	private Vector3 smoothedPos;
+
+	private void Update()
+	{
+		if(target)
+		{
+			desiredPos = target.position + offset;
+			smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothing * Time.deltaTime);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if(target)
+			transform.position = smoothedPos;
+	}
+}
