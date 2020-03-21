3206e20c7ad8b5005926f8916e5a9dbed58122a8
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-21 16:49:51 +0100

    Fix bad cloud jumper character movement

--- Assets/Scripts/CloudJumperCharacterController.cs
+++ Assets/Scripts/CloudJumperCharacterController.cs
@@ -8,6 +8,9 @@ public class CloudJumperCharacterController : MonoBehaviour
 	[SerializeField] private Rigidbody2D rb = default;
 	[SerializeField] private float moveDir = 0;
 	[SerializeField] private float movespeed = 10000;
+	[SerializeField] private float maxVelocity;
+	[SerializeField] private float jumpCooldown = 1.5f;
+	[SerializeField] private bool canJump = true;
 	[Space]
 	[SerializeField] private Transform bottomOfChar = default;
 
@@ -19,14 +22,13 @@ public class CloudJumperCharacterController : MonoBehaviour
 		moveDir = GyroControl.Instance.NewRotation;
 		Vector3 pos = transform.position;
 
+		rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+
 		float dist = (transform.position - Camera.main.transform.position).z;
 		float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
 		float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
 
-		if(moveDir > -0.8)
-			rb.AddForce(Vector3.right * movespeed * Time.deltaTime);
-		if(moveDir < 0.8)
-			rb.AddForce(-Vector3.right * movespeed * Time.deltaTime);
+		transform.Translate(new Vector3(moveDir * movespeed * Time.deltaTime, 0, 0));
 
 		pos.x = Mathf.Clamp(transform.position.x, leftBorder, rightBorder);
 		transform.position = pos;
@@ -34,10 +36,18 @@ public class CloudJumperCharacterController : MonoBehaviour
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		if(collision.gameObject.CompareTag("Cloud"))
+		if(collision.gameObject.CompareTag("Cloud") && canJump)
 		{
 			rb.AddForce(Vector3.up * jumpForce * Time.deltaTime, ForceMode2D.Impulse);
+			StartCoroutine(StartJumpCooldown());
 			Debug.Log("Jumped!");
 		}
+	}
+
+	private IEnumerator StartJumpCooldown()
+	{
+		canJump = false;
+		yield return new WaitForSeconds(jumpCooldown);
+		canJump = true;
 	}
 }
