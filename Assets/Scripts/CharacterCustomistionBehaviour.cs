164c98340276a53391fcc58661218c036ddcad0a
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-22 22:47:16 +0100

    Add new Assets

---
+++ Assets/Scripts/CharacterCustomistionBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCustomistionBehaviour : MonoBehaviour
+{
+	[SerializeField] private Transform torsoTransform = default;
+	[SerializeField] private Transform lArmTransform = default;
+	[SerializeField] private Transform RArmTransform = default;
+	[SerializeField] private Transform lLegTransform = default;
+	[SerializeField] private Transform rLegTransform = default;
+}
